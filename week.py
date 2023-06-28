c760d57fb953c9af0fe678aca46a48a9df28058a
agweb <aridding18@gmail.com>
2023-06-27 20:14:22 -0400

    Update week with visualization

--- week.py
+++ week.py
@@ -12,18 +12,14 @@ import matplotlib.pyplot as plt
 file_name = 'vgsales.csv'
 
 
-def business_problem(_topic):
+def business_problem(_topic, _image_file_name):
     raw_df = pd.read_csv(file_name)
     publisher_df = raw_df.groupby([_topic])[['Global_Sales', 'JP_Sales']].sum().reset_index()
     publisher_df = publisher_df.sort_values('Global_Sales', ascending=False)
-    return publisher_df
+    fig, ax = plt.subplots()
+    ax.plot(year_analysis[_topic].values, year_analysis['Global_Sales'].values)  # Create a figure containing a single axes.
+    plt.savefig(_image_file_name)
+    return year_analysis
 
-year_analysis = business_problem('Year')
-platform_analysis = business_problem('Platform')
-
-
-year_analysis = year_analysis.sort_values(['Year'])
-fig, ax = plt.subplots()
-ax.plot(year_analysis['Year'].values, year_analysis['Global_Sales'].values)  # Create a figure containing a single axes.
-plt.show()
-plt.savefig('videogame.png')
+#   Called the function, grouped on "Year"
+year_analysis = business_problem('Year', 'videoanalysis.png')
